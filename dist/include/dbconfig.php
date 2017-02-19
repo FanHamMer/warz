1adff94c562d5ffdbb83869018ad0e75dcc862eb
Mingeun Kim <kmg@securityplus.or.kr>
2017-02-20 02:11:07 +0900

    Revert "warz"
    This reverts commit 710f79067726e02a7ffb398fc902f756a1a2f62a.

--- dist/include/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $connect = mysql_connect('localhost','root','') or die ('db error');
-  mysql_set_charset("utf8");
-  mysql_select_db('warzdb',$connect) or die('db error2');
-  mysql_query('set names utf8');
-  ?>
